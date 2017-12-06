768b53505600659dae7919a00e1271111511ddd9
AgungDw <pif.zwei@gmail.com>
2017-12-06 22:42:29 +0700

    mv: fixing some bugs, rm: add -rf

--- rm.c
+++ rm.c
@@ -9,21 +9,94 @@
 #include <xv6/fcntl.h>
 #include <xv6/fs.h>
 
-void removethis(char*argv)
+
+int test_dir (char * path)
 {
-	int fd;
-  	fd = open(argv, 0);
-  	if(fd >= 0){
-    	while(1)
-   	unlink(argv);
- 	}
+  int fd;
+  fd =open (path,O_RDONLY);
+  if (fd<0)
+  {
+    // printf("cannot open path: %s\n", path);
+    // sysexit();
+    // exit();
+    return -1;
+  }
+
+  struct stat st;
+  if(fstat(fd, &st) < 0){
+    // printf("ls: cannot stat %s\n", path);
+    // close(fd);
+    // sysexit();
+    // exit();
+    return -1;
+  }
+  if (st.type == T_FILE)
+  {
+    return 1;
+  }
+  else if (st.type == T_DIR)
+  {
+    return 0;
+  }
+  else 
+    return -1;
 }
 
+void removethis(char*argv)
+{
+	if(unlink(argv) < 0)
+  {
+    printf("rm: failed to delete:%s\n", argv);
+  }
+}
 
+void wildcard (char * path)
+{
+  
+  int fd;
+  fd =open (path,0);
+  if (fd<0)
+  {
+    printf("cannot open path: %s\n", path);
+    return; 
+  }
+  struct dirent looker;
+  char * walker;  
+  char buff_src [512];
+  while (read(fd, &looker, sizeof(looker)) == sizeof(looker))
+  {  
+    strcpy(buff_src, path);
+    if (strcmp(looker.d_name, ".") ==0 || strcmp(looker.d_name, "..") ==0)
+      continue;
+    
+    // printf("%s %s %s\n",  buff_src, buff_dest, looker.d_name);
+  
+    walker= buff_src + strlen(buff_src);
+    if (*walker != '/') strcat(buff_src, "/");
+    // printf("%s %s %s\n",  buff_src, looker.d_name);
+    strcat(buff_src, looker.d_name);
+    strcat(buff_src, "\0");
+    // printf("%s %s %s\n",  buff_src, buff_dest, looker.d_name);
+    int test=test_dir (buff_src);
+    if (test == 0)
+    {
+      wildcard(buff_src);
+    }
+    else if (test == 1)
+      removethis(buff_src);
+    else 
+    {
+      printf("rm: cannot open/stat %s\n", buff_src);
+      sysexit();
+    }
+  }
+  if(unlink(path) < 0)
+  {
+    printf("rm: failed to delete directory:%s\n", buff_src);
+  }
+}
 
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i;
 
@@ -31,13 +104,13 @@ main(int argc, char *argv[])
     printf( "Usage: rm files...\n");
     sysexit();
   }
-
-  for(i = 1; i < argc; i++){
-    if(unlink(argv[i]) < 0){
-      printf("rm: %s failed to delete\n", argv[i]);
-      break;
-    }
+  if (strcmp(argv[1],"-rf")==0)
+  {
+    wildcard(argv[2]);
+    sysexit();
   }
-
+  for(i = 1; i < argc; i++){
+    removethis(argv[i]);
+  }
   sysexit();
 }
